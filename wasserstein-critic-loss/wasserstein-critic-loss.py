54f1240b3eeb99e4996c65d7e2a80020b0d46ac1
Kedar Hukkeri <kedarhukkeri2004@gmail.com>
2026-02-26 11:04:31 +0530

    Add solution for wasserstein-critic-loss

---
+++ wasserstein-critic-loss/wasserstein-critic-loss.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def wasserstein_critic_loss(real_scores, fake_scores):
+    real_scores = np.array(real_scores, dtype=float)
+    fake_scores = np.array(fake_scores, dtype=float)
+    
+    mean_real = np.mean(real_scores)
+    mean_fake = np.mean(fake_scores)
+    
+    return float(mean_fake - mean_real)
